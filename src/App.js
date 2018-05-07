8f4ef0f60f5d128e59a7886384123fd443a53ca7
Nicolas Laing <nicolaslaing@nal9.fios-router.home>
2018-05-07 11:05:49 -0400

    App testing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
+/* global it, expect */
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
+});
