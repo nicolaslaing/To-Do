8481626e6277ceddc4d2e8292144b74e6d56f20a
Nicolas Laing <nicolaslaing@nal9.fios-router.home>
2018-05-07 16:27:16 -0400

    Extraneous code removal

--- src/App.js
+++ src/App.js
@@ -12,34 +12,3 @@ const App = () => (
 )
 
 export default App
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Todo App</h1>
-//         </header>
-//         <p>
-//           Hello! It's time to check off some tasks...
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-// export default App;
-
-// import React, { Component } from 'react';
-
-// const App = () => (
-//   <div>
-//     My App
-//   </div>
-// );
-
-// export default App;
